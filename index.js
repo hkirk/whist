e632f142c26f2249fdfc8a6bf91f376e20a9f4af
Claus Andersen <clan82@gmail.com>
2012-08-18 18:19:33 +0200

    JS i separat fil. Melding/stik selects under tabel Baggrund paa bidder team members

---
+++ index.js
@@ -0,0 +1,275 @@
+$(function() {
+
+	var G = 1;
+	var round = 0;
+	// Id's are not used yet
+	var extras = [
+	{
+		id: 'none', 		
+		name: null,   		
+		multiplier: 1
+	},
+	{
+		id: 'vip',  
+		name: 'vip',  
+		multiplier: 1.5
+	}, 
+	{
+		id: 'gode', 
+		name: 'gode', 
+		multiplier: 2
+	}
+	];
+	var solos = [
+	{
+		id: 'sol',     
+		name: 'Sol',        
+		multiplier: 1, 
+		maxTricks: 1
+	},
+	{
+		id: 'rensol',  
+		name: 'Ren Sol',    
+		multiplier: 2, 
+		maxTricks: 0
+	},
+	{
+		id: 'bord',    
+		name: 'Bordl.',     
+		multiplier: 4, 
+		maxTricks: 1
+	},
+	{
+		id: 'renbord', 
+		name: 'Ren Bordl.', 
+		multiplier: 8, 
+		maxTricks: 0
+	}
+	];
+	var totalPoints = [ 0, 0, 0, 0 ];
+	var lastPoints = [ null, null, null, null ];
+	var isRoundCalculated = true; // pseudo round 0 is calculated to totalPoints
+
+	function addRound() {	
+		if(!isRoundCalculated) {
+			alert("Round is not calculated!");
+			return;
+		}
+		round++;
+		var html = "<tr id='round-" + round + "'>";
+		html += "<th class='round'>" + round + "</th>";
+		html += "<td class='bid-tricks'></td>";
+		//html += "<td class='tricks'></td>"
+		for (var player=0; player<4; player++) {
+			html += "<td class='result result-"+player+"'></td>"
+			html += "<td class='total total-"+player+"'></td>";
+		}
+		html += "</tr>";
+		$('tbody').append(html);
+		// Reset controls
+		$("#bid").val("");
+		$("#tricks").val("");
+		for(var p=0;p<4;p++) {
+			$("#bidderteam-"+p).prop('checked',false);
+		}
+		lastPoints = totalPoints.slice(0); // clone
+		isRoundCalculated = false;
+	}
+	
+	function getNormalBidText(bidTricks, extraIndex) {
+		var extra = extras[extraIndex];
+		return bidTricks + (extra.name ? " " + extra.name : "")
+	}
+	
+	function getSoloBidText(soloIndex) {
+		var solo = solos[soloIndex];
+		return solo.name;
+	}
+			
+	function updateTotalPoints(currentPoints) {
+		for(var p=0;p<4;p++) {
+			var points = totalPoints[p] = lastPoints[p] + currentPoints[p];
+			$('#total-'+p).text(points);
+		}
+	}
+			
+	function populateBidSelect() {
+		var select;
+		select = getOptionAdder("#bid-normal");
+		for (var i = 7; i <= 13; i++) {
+			$(extras).each(function(index,extra) {
+				var points = calculateNormal(i, i, extra);
+				var text = getNormalBidText(i, index);
+				select.addOption(i + "-" + index, text + " ("+points+")");
+			});
+		}
+		select = getOptionAdder("#bid-solo");
+		$(solos).each(function(index, solo) {
+			var points = calculateSolo(0, solo);
+			var text = getSoloBidText(index);
+			select.addOption('solo-'+index, text + " ("+points+")");
+		});
+	}
+			
+	function populateTrickSelect() {
+		var select = getOptionAdder("#tricks");
+		for (var i = 0; i <= 13; i++) {
+			select.addOption(i, i);
+		}
+	}
+			
+	function getOptionAdder(selector) {
+		var element = $(selector);
+		element.addOption = function(value, text) {
+			createOption(value, text, this);
+		}
+		return element;
+	}
+			
+	function createSelect(id) {
+		var select = $("<select/>", {
+			id: id
+		});
+		select.addOption = function(value, text) {
+			createOption(value, text, this);
+		}
+		return select;
+	}
+			
+	function createOption(value,text,select) {
+		var option = $("<option/>", {
+			value: value,
+			text: text
+		});
+		select &&	select.append(option);
+		return option;
+	}
+			
+	/*
+			 * 
+			 */
+	function calculatePoints() {
+		if(round<1) {
+			alert("No latest round!");
+			return;
+		}
+
+		var bidTemp = $("#bid").val();
+		var tricks = $("#tricks").val();
+		if(bidTemp===''||tricks==='') {
+			alert("Missing input!");
+			return;
+		}
+		var splittedBidTemp = bidTemp.split("-");
+		var bidTricks = splittedBidTemp[0];
+		var extraOrSoloIndex = splittedBidTemp[1];
+		var isSolo = bidTricks==='solo';
+
+		var bidders = getBidderTeamPlayers();
+		if(bidders.length===0) {
+			alert("Missing bidder(s)!");
+			return;
+		}
+				
+		var points = [null, null, null, null];
+		// Note: bidTricks, tricks and extraOrSoloIndex are strings, but are coerced into numbers(ints) in the formula
+		var result = 0;
+		if (isSolo) { // sol
+			result = calculateSolo(tricks, solos[extraOrSoloIndex]);
+		} else { // normal
+			result = calculateNormal(bidTricks, tricks, extras[extraOrSoloIndex]);
+		}				
+		// Initialize all players points to opponent team points
+		var opponentTeamPoints = result * -1;
+		for(var p=0;p<4;p++) {
+			points[p] = opponentTeamPoints;
+		}
+				
+		if(isSolo) {
+			switch(bidders.length) {
+				case 1 :
+					// Set lonely bidder points
+					points[bidders[0]] = result * 3;
+					break;
+				default:
+					alert("We don't support more than one solo-player :(");
+					return;
+			}					
+		} else { // normal
+			switch(bidders.length) {
+				case 1 :
+					// Set lonely bidder points
+					points[bidders[0]] = result * 3;
+					break;
+				case 2 :
+					// Set "normal" game bidders points
+					points[bidders[0]] = points[bidders[1]] = result;
+					break;
+				default:
+					alert("Too many bidders!");
+					return;
+			}
+		}
+		updateTotalPoints(points, row);
+		var row = $("tr#round-"+round);
+		for(var p=0;p<4;p++) {
+			var isOpponent = bidders.indexOf(p)<0;
+			var td = row.find("td.result-"+p);
+			td.text(points[p]);
+			toggleClass(td, points[p]<0.0, "negative", "positive");
+			toggleClass(td, isOpponent, "opponent-team", "bidder-team");
+			td = row.find("td.total-"+p);
+			td.text(totalPoints[p]);
+			toggleClass(td, isOpponent, "opponent-team", "bidder-team");
+		}
+		var bidTricksText;
+		if(isSolo) {
+			bidTricksText = getSoloBidText(extraOrSoloIndex);
+		} else {
+			bidTricksText = getNormalBidText(bidTricks, extraOrSoloIndex);
+		}
+		row.find("td.bid-tricks").text(bidTricksText+" / "+tricks);
+		isRoundCalculated = true;
+	}
+	
+	function toggleClass(element, bool, trueClass, falseClass) {
+		if(bool) {
+			element.removeClass(falseClass).addClass(trueClass);
+		} else {
+			element.removeClass(trueClass).addClass(falseClass);
+		}
+	}
+			
+	function getBidderTeamPlayers() {
+		var bidders = [];
+		for(var player=0; player<4; player++) {
+			if($("#bidderteam-"+player).prop('checked')) {
+				bidders.push(player);
+			}
+		}
+		return bidders;
+	}
+
+	function calculateSolo(tricks, type) {
+		var S = G * 6;
+		var looserMultiplier = tricks > type.maxTricks ? -2 : 1;
+		return S * type.multiplier * looserMultiplier;
+	}
+
+	function calculateNormal(bid, tricks, extra) {
+		var looserMultiplier = bid > tricks ? 2 : 1;
+		var looserDisplacement = bid > tricks ? 0 : 1;
+		//alert("bid: "+bid+", tricks: "+tricks+", lm: "+looserMultiplier+", ld: "+looserDisplacement+", em: "+extra.multiplier);
+		return G * Math.pow(2, bid-7) * extra.multiplier * looserMultiplier * (tricks-bid+looserDisplacement);
+	}
+			
+	function init() {
+		populateBidSelect();
+		populateTrickSelect();
+		addRound();
+		$("#addRound").click(addRound);
+		$("#calculatePoints").click(calculatePoints);
+	}
+			
+	init();
+});
--- index.php
+++ index.php
@@ -1,288 +1,69 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link type="text/css" href="index.css" rel="stylesheet" media="all" />
-		<script src="http://code.jquery.com/jquery-latest.js"></script>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Whist calculator</title>
-		<script type="text/javascript">
-			var G = 1;
-			var round = 0;
-			// Id's are not used yet
-			var extras = [
-				{ id: 'none', name: null,   multiplier: 1   },
-				{ id: 'vip',  name: 'vip',  multiplier: 1.5 }, 
-				{ id: 'gode', name: 'gode', multiplier: 2   }
-			];
-			var solos = [
-				{ id: 'sol',     name: 'Sol',            multiplier: 1, maxTricks: 1 },
-				{ id: 'rensol',  name: 'Ren Sol',        multiplier: 2, maxTricks: 0 },
-				{ id: 'bord',    name: 'Bordlægger',     multiplier: 4, maxTricks: 1 },
-				{ id: 'renbord', name: 'Ren Bordl.', multiplier: 8, maxTricks: 0 }
-			];
-			var totalPoints = [ 0, 0, 0, 0 ];
-			var lastPoints = [ null, null, null, null ];
-			var isRoundCalculated = true; // pseudo round 0 is calculated to totalPoints
-
-			function addRound() {
-				if(!isRoundCalculated) {
-					alert("Round is not calculated!");
-					return;
-				}
-				round++;
-				var html = "<tr id='round-" + round + "' class=\"" + round + "\">";
-				html += "<th class='round'>" + round + "</th>";
-				html += "<td class='bid'></td>";
-				html += "<td class='tricks'></td>"
-				for (var player=0; player<4; player++) {
-					html += "<td class='result result-"+player+"'></td>"
-					html += "<td class='total total-"+player+"'></td>";
-				}
-				html += "</tr>";
-				$('tbody').append(html);
-				// Reset controls
-				$("#bid").val("");
-				$("#tricks").val("");
-				for(var p=0;p<4;p++) {
-					$("#bidderteam-"+p).prop('checked',false);
-				}
-				lastPoints = totalPoints.slice(0); // clone
-				isRoundCalculated = false;
-			}
-			
-			function updateTotalPoints(currentPoints, row) {
-				for(var p=0;p<4;p++) {
-					var points = totalPoints[p] = lastPoints[p] + currentPoints[p];
-					$('#total-'+p).text(points);
-					row.find(".total-"+p).text(points);
-				}
-			}
-			
-			function populateBidSelect() {
-				var select = getSelect("#bid");
-				for (var i = 7; i <= 13; i++) {
-					$(extras).each(function(index,extra) {
-						var points = calculateNormal(i, i, extra);
-						select.addOption(i + "-" + index, i + (extra.name ? " " + extra.name : "") + " ("+points+")");
-					});
-				}
-				$(solos).each(function(index, solo) {
-					var points = calculateSolo(0, solo);
-					select.addOption('solo-'+index, solo.name + " ("+points+")");
-				});
-			}
-			
-			function populateTrickSelect() {
-				var select = getSelect("#tricks");
-				for (var i = 0; i <= 13; i++) {
-					select.addOption(i, i);
-				}
-			}
-			
-			function getSelect(selector) {
-				var select = $(selector);
-				select.addOption = function(value, text) {
-					createOption(value, text, this);
-				}
-				return select;
-			}
-			
-			function createSelect(id) {
-				var select = $("<select/>", {
-					id: id
-				});
-				select.addOption = function(value, text) {
-					createOption(value, text, this);
-				}
-				return select;
-			}
-			
-			function createOption(value,text,select) {
-				var option = $("<option/>", {
-					value: value,
-					text: text
-				});
-				select &&	select.append(option);
-				return option;
-			}
-			
-			/*
-			 * 
-			 */
-			function calculatePoints() {
-				if(round<1) {
-					alert("No latest round!");
-					return;
-				}
-
-				var bidSelect = $("#bid");
-				var bid = bidSelect.val();
-				var bidText = bidSelect.find(":selected").text();
-				var tricksSelect = $("#tricks");
-				var tricks = tricksSelect.val();
-				var tricksText = tricksSelect.find(":selected").text();
-				if(bid===''||tricks==='') {
-					alert("Missing input!");
-					return;
-				}
-				var temp = bid.split("-");
-				var result = 0;
-				var bidders = getBidderTeamPlayers();
-				if(bidders.length===0) {
-					alert("Missing bidder(s)!"); return;
-				}
-				var isSolo = temp[0]==='solo';
-				
-				var points = [null, null, null, null];
-				// Note temp[0] and temp[1] are strings, but are coerced into numbers in the formula
-				if (isSolo) { // sol
-					result = calculateSolo(tricks, solos[temp[1]]);
-				} else { // normal
-					result = calculateNormal(temp[0], tricks, extras[temp[1]]);
-				}				
-				// Initialize all players points to non-bidders points
-				for(var p=0;p<4;p++) {
-					points[p] = result * -1;
-				}
-				
-				if(isSolo) {
-					switch(bidders.length) {
-						case 1 : 
-							// Set lonely bidder points
-							points[bidders[0]] = result * 3;
-							break;
-						default: alert("We dont support more than one solo-player :("); return;
-					}					
-				} else { // normal
-					switch(bidders.length) {
-						case 1 : 
-							// Set lonely bidder points
-							points[bidders[0]] = result * 3;
-							break;
-						case 2 :
-							// Set "normal" game bidders points
-							points[bidders[0]] = points[bidders[1]] = result;
-							break;
-						default: alert("Too many bidders!"); return;
-					}
-				}
-				var row = $("tr#round-"+round);
-				for(var p=0;p<4;p++) {
-					var td = row.find("td.result-"+ p);
-					td.text(points[p]);
-					var removeClass = points[p]<0.0?"positive":"negative";
-					var addClass = points[p]<0.0?"negative":"positive";
-					td.removeClass(removeClass).addClass(addClass);
-					td.removeClass("bidderteam"); // default is not bidder team
-				}
-//				$(bidders).each(function(index,player) {
-//					var td = row.find("td.result-"+ player);
-//					td.addClass("bidderteam");
-//				});
-				row.find("td.bid").text(bidText);
-				row.find("td.tricks").text(tricksText);
-				updateTotalPoints(points, row);
-				isRoundCalculated = true;
-			}
-			
-			function getBidderTeamPlayers() {
-				var bidders = [];
-				for(var player=0; player<4; player++) {
-					if($("#bidderteam-"+player).is(':checked')) {
-						bidders.push(player);
-					}
-				}
-				return bidders;
-			}
-			/*
-			 * Sol:
-					 S = soltakst = G * 2^(9-7) *1,5 = G * 6
-					 t = soltype. Beskidt = 1, Ren =2, Oplægger =4, Ren oplægger = 8
-					 v = væltet? Nej = 1, Ja = -2
-            
-					 Syg formel:
-					 S * t * v  (*3...)
-			 */
-			function calculateSolo(tricks, type) {
-				var S = G * 6;
-				var looserMultiplier = tricks > type.maxTricks ? -2 : 1;
-				return S * type.multiplier * looserMultiplier;
-			}
-			/*whist
-			 * G = Grundtakst = 1
-					 n = antal stik meldt [7-13] (7 = mindste melding)
-					 m = antal stik fået [0-13]
-					 p = påhæng? Intet = 1, Med påhæng = 2 (tæller vip med?)
-					 b = bet? (m-n<0?). Nej = 1, Ja = 2
-					 f = bet-forskydning. Bet? Nej = 1, Ja = 0
-            
-					 Den sygeste formel:
-					 G * 2^(n-7) * p *  b * (m - n + f)
-			 */
-			function calculateNormal(bid, tricks, extra) {
-				var looserMultiplier = bid > tricks ? 2 : 1;
-				var looserDisplacement = bid > tricks ? 0 : 1;
-				//alert("bid: "+bid+", tricks: "+tricks+", lm: "+looserMultiplier+", ld: "+looserDisplacement+", em: "+extra.multiplier);
-				return G * Math.pow(2, bid-7) * extra.multiplier * looserMultiplier * (tricks-bid+looserDisplacement);
-			}
-			
-			function init() {
-				populateBidSelect();
-				populateTrickSelect();
-				addRound();
-			}
-			
-			$(function() {
-				init();			
-			});
-		</script>
+		<link type="text/css" href="index.css" rel="stylesheet" media="all" />
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script type="text/javascript" src="index.js"></script>
 	</head>
 	<body>
 		<h1>The funky Whist Calculator</h1>
-		<table class="results">
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Melding</th>
-					<th>Stik</th>
-					<?php for ($player = 1; $player <= 4; $player++) { ?>
-						<th colspan='2'>
-							Player <?= $player ?> <br />
-							<input type="text" size="5" name="player[<?= $player ?>]" />
-						</th>
-					<?php } ?>
-				</tr>
-			</thead>
-			<tbody>
-			</tbody>
-			<tfoot>
-				<tr id="controls">
-					<td colspan="3">
-						<select id="bid">
-							<option value="">Melding</option>
-						</select>
-						/
-						<select id="tricks">
-							<option value="">Stik</option>
-						</select>
-					</td>
-					<?php for ($p = 0; $p < 4; $p++) { ?>
-						<td colspan="2">
-							<input type="checkbox" id="bidderteam-<?= $p ?>" />
-						</td>
-					<?php } ?>
-				</tr>
-				<tr id="total">
-					<th colspan="3">Total:</th>
-					<?php for ($p = 0; $p < 4; $p++) { ?>
-						<th colspan='2' id='total-<?= $p ?>'> </th>
-					<?php } ?>
-				</tr>				
-			</tfoot>
-		</table>
+		<div>
+			<table class="results">
+				<thead>
+					<tr>
+						<th class="round">#</th>
+						<th>Melding / Stik</th>
+						<?php for ($player = 1; $player <= 4; $player++) { ?>
+							<th colspan='2'>
+								Pl. <?= $player ?> <br />
+								<input type="text" size="3" name="player[<?= $player ?>]" />
+							</th>
+						<?php } ?>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+				<tfoot>
+					<tr id="bidderteam">
+						<th rowspan="2" class="round">#</th>
+						<th class="bid-tricks">Melderhold:</th>
+						<?php for ($p = 0; $p < 4; $p++) { ?>
+							<td colspan="2">
+								<input type="checkbox" id="bidderteam-<?= $p ?>" />
+							</td>
+						<?php } ?>
+					</tr>
+					<tr id="total">
+						<th>Total:</th>
+						<?php for ($p = 0; $p < 4; $p++) { ?>
+							<th colspan='2' id='total-<?= $p ?>'> </th>
+						<?php } ?>
+					</tr>				
+				</tfoot>
+			</table>
+		</div>
+		<div id="bid-tricks-container">
+			<span>
+				<label for="bid">Melding:</label>
+				<select id="bid">
+					<option value="">Melding</option>
+					<optgroup label="Normal" id="bid-normal"></optgroup>
+					<optgroup label="Solo" id="bid-solo"></optgroup>
+				</select>
+			</span>
+			/
+			<span>
+				<label for="tricks">Stik:</label>
+				<select id="tricks">
+					<option value="">Stik</option>
+				</select>
+			</span>
+		</div>
 		<div id="buttons-container">
-			<input type="button" value="Add round" onclick="addRound()"/>
-			<input type="button" value="Calculate latest round" onclick="calculatePoints()" />
+			<button id="addRound">Add round</button>
+			<button id="calculatePoints">Calculate latest round</button>
 		</div>
 	</body>
 </html>
