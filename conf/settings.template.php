8f20f714827518ca3d8b9f9eb569976a7b4fef76
Claus Andersen <clan82@gmail.com>
2013-02-24 22:14:47 +0100

    Settings moved to conf/ and as template file.

---
+++ conf/settings.template.php
@@ -0,0 +1,14 @@
+<?php
+
+$SETTINGS = array(
+		// Base URL for the calculator. No trailing slash!
+		"base_url" => "http://localhost/whist",
+		// MySQL database settings:
+		"database" => array(
+				"host" => "localhost",
+				"name" => "whist_calc",
+				"username" => "username",
+				"password" => "password"
+		)
+);
+
--- lib/lib.php
+++ lib/lib.php
@@ -1,7 +1,12 @@
 <?php
 
 $lib_dir = dirname(__FILE__);
-require($lib_dir . "/settings.php");
+$conf_dir = $lib_dir . "/../conf";
+
+// Config files:
+require($conf_dir . "/settings.php");
+
+// Lib files:
 require($lib_dir . "/core.php");
 require($lib_dir . "/db/database.php");
 require($lib_dir . "/util.php");
