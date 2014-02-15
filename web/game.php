acd06464d94ee681c5bb9140138560110702e4ec
Claus Andersen <clan82@gmail.com>
2014-02-15 13:22:27 +0100

    Using data from the last round to get the active round, and therefore avoiding the extra db_get_game_type_with_active_round() query(ies). Time data on rounds. Prepared for auto bye players.

--- web/game.php
+++ web/game.php
@@ -47,59 +47,6 @@ foreach ($db_game['players'] as $player) {
 $n_players = count($players);
 $point_rules = &$db_game['point_rules'];
 
-$db_game_with_active_round = db_get_game_type_with_active_round($id);
-
-$active_round = $db_game_with_active_round['active_round'];
-
-if ($active_round === NULL) {
-	$controls_view = 'beginround';
-	$cancel_view = NULL;
-	$legal_attachment_keys = [];
-	foreach ($ATTACHMENT_KEY_ORDER as $attachment_key) {
-		if (in_array($attachment_key, $REQUIRED_ATTACHMENT_KEYS_ORDER) || in_array($attachment_key, $db_game['attachments'])) {
-			$legal_attachment_keys[] = $attachment_key;
-		}
-	}
-	$is_tips_legal = in_array(TIPS, $legal_attachment_keys);
-	$tips_count = in_array(POINT_RULE_TIPS, $point_rules);
-	//printf("Tips count: %s",$tips_count);
-	$cancel_view_data = NULL;
-	$controls_view_data = [
-			'is_tips_legal' => $is_tips_legal,
-			'tips_count' => $tips_count,
-			'legal_attachment_keys' => $legal_attachment_keys
-	];
-} else {
-	$controls_view = 'endround';
-	$cancel_view = 'cancelactiveround';
-	$bid_type = $active_round['bid_type'];
-	$bid_data = $active_round['bid_data'];
-	if ($bid_type === 'normal') {
-		$bid_winner_positions = [$bid_data['bid_winner_position']];
-	} else {
-		$bid_winner_positions = array_keys($bid_data['bid_winner_tricks_by_position']);
-	}
-	$bye_player_positions = [];
-	$participating_player_positions = [];
-	foreach ($active_round['player_data'] as $position => $data) {
-		if ($data['is_bye']) {
-			$bye_player_positions[] = $position;
-		} else {
-			$participating_player_positions[] = $position;
-		}
-	}
-	$cancel_view_data = [
-			'game_id' => &$id
-	];
-	$controls_view_data = [
-			'bid_type' => $bid_type,
-			'bid_winner_positions' => $bid_winner_positions,
-			'bye_player_positions' => $bye_player_positions,
-			'participating_player_positions' => $participating_player_positions
-	];
-	//error_log(print_r($db_game_with_active_round, true));
-}
-
 
 
 try {
@@ -109,13 +56,14 @@ try {
 	game_render_error('bad_rounds');
 }
 
-$acc_total_points = array_fill(0, $n_players, 0);
-
-$rounds = [];
 
 //
 // Build round information for each round
 //
+
+$acc_total_points = array_fill(0, $n_players, 0);
+$rounds = [];
+
 foreach ($db_rounds as $r) {
 	$bid_type = $r['bid_type'];
 	$data = $r['bid_data'];
@@ -150,10 +98,70 @@ foreach ($db_rounds as $r) {
 			'player_data' => $player_data,
 			'bid' => $bid,
 			'bid_winner_tricks_by_position' => $bid_winner_tricks_by_position,
-			'bid_winner_mate_position' => $bid_winner_mate_position
+			'bid_winner_mate_position' => $bid_winner_mate_position,
+			'started_at' => $r['started_at'],
+			'ended_at' => $r['ended_at'],
+			'updated_at' => $r['updated_at']
 	];
 	$rounds[] = $round;
 }
+
+$n_rounds = count($rounds);
+
+
+
+//
+// Build begin or end round data, based on the most recent round
+//
+
+$active_round = NULL;
+if ($n_rounds > 0 && $rounds[$n_rounds - 1]['ended_at'] === NULL) {
+	$active_round = $rounds[$n_rounds - 1];
+}
+
+if ($active_round === NULL) {
+	$controls_view = 'beginround';
+	$cancel_view = NULL;
+	$legal_attachment_keys = [];
+	foreach ($ATTACHMENT_KEY_ORDER as $attachment_key) {
+		if (in_array($attachment_key, $REQUIRED_ATTACHMENT_KEYS_ORDER) || in_array($attachment_key, $db_game['attachments'])) {
+			$legal_attachment_keys[] = $attachment_key;
+		}
+	}
+	$is_tips_legal = in_array(TIPS, $legal_attachment_keys);
+	$tips_count = in_array(POINT_RULE_TIPS, $point_rules);
+	$cancel_view_data = NULL;
+	$controls_view_data = [
+			'is_tips_legal' => $is_tips_legal,
+			'tips_count' => $tips_count,
+			'legal_attachment_keys' => $legal_attachment_keys
+	];
+} else {
+	$controls_view = 'endround';
+	$cancel_view = 'cancelactiveround';
+	$bid_type = $active_round['bid']['type'];
+	$bid_winner_positions = array_keys($active_round['bid_winner_tricks_by_position']);
+	$bye_player_positions = [];
+	$participating_player_positions = [];
+	foreach ($active_round['player_data'] as $position => $data) {
+		if ($data['is_bye']) {
+			$bye_player_positions[] = $position;
+		} else {
+			$participating_player_positions[] = $position;
+		}
+	}
+	$cancel_view_data = [
+			'game_id' => &$id
+	];
+	$controls_view_data = [
+			'bid_type' => $bid_type,
+			'bid_winner_positions' => $bid_winner_positions,
+			'bye_player_positions' => $bye_player_positions,
+			'participating_player_positions' => $participating_player_positions
+	];
+}
+
+
 /*
   printf("DB:");
   var_dump($db_rounds);
