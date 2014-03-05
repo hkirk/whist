1d2f410dd47cc537da89806c3502b1b0c22fc07a
Henrik Kirk <henrik@busywait.org>
2014-03-05 20:59:00 +0100

    Updated with bootstrap css

--- lib/views/layout.php
+++ lib/views/layout.php
@@ -6,6 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="styles/style.css" />
 
