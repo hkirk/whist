3af6d606975a6d1b16a7f17a302485f8a3d649f1
Claus Andersen <clan82@gmail.com>
2014-03-15 11:41:27 +0100

    Grouped <script>s after <link>s in HTML <head>.

--- lib/views/layout.php
+++ lib/views/layout.php
@@ -6,10 +6,10 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="styles/style.css" />
 
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
